88576c067ddc09efcf2653630eb491a0785b446f
Len Li <leonard.li@gmail.com>
2014-04-30 15:55:40 -0400

    Add DataManager

---
+++ Alice/LCLTicketDataManager.h
@@ -0,0 +1,23 @@
+//
+//  LCLTicketDataManager.h
+//  Alice
+//
+//  Created by Leonard Li on 4/30/14.
+//  Copyright (c) 2014 Leonard Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LCLTicketDataStore.h"
+#import "LCLAliceAPIClient.h"
+
+@interface LCLTicketDataManager : NSObject
+
+@property (strong, nonatomic) LCLAliceAPIClient *aliceAPIClient;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) LCLTicketDataStore *dataStore;
+
++ (instancetype)sharedManager;
+- (instancetype)initWithAliceAPIClient:(LCLAliceAPIClient *)aliceClient
+                  managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+                             dataStore:(LCLTicketDataStore *)dataStore;
+@end
---
+++ Alice/LCLTicketDataManager.m
@@ -0,0 +1,64 @@
+//
+//  LCLTicketDataManager.m
+//  Alice
+//
+//  Created by Leonard Li on 4/30/14.
+//  Copyright (c) 2014 Leonard Li. All rights reserved.
+//
+
+#import "LCLTicketDataManager.h"
+
+@implementation LCLTicketDataManager
+
++ (LCLAliceAPIClient *)defaultAliceClient
+{
+    return [LCLAliceAPIClient sharedClient];
+}
+
++ (NSManagedObjectContext *)defaultManagedObjectContext
+{
+    return [LCLTicketDataStore sharedDataStore].managedObjectContext;
+}
+
++ (LCLTicketDataStore *)defaultDataStore
+{
+    return [LCLTicketDataStore sharedDataStore];
+}
+
+
+#pragma mark - Init Helper Methods
+
++ (instancetype)sharedManager {
+    
+    return [[self alloc] init];
+}
+
+- (instancetype)init
+{
+    return [self initWithAliceAPIClient:[[self class] defaultAliceClient]
+                   managedObjectContext:[[self class] defaultManagedObjectContext]
+                              dataStore:[[self class] defaultDataStore]];
+
+}
+
+- (instancetype)initWithAliceAPIClient:(LCLAliceAPIClient *)aliceClient
+                  managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+                             dataStore:(LCLTicketDataStore *)dataStore
+{
+    
+    static LCLTicketDataManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [super init];
+        if (_sharedManager) {
+            _aliceAPIClient = aliceClient;
+            _managedObjectContext = managedObjectContext;
+            _dataStore = dataStore;
+        }
+    });
+    
+    return _sharedManager;
+}
+
+
+@end
