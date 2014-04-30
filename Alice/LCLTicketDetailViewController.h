2a13e0513f4aa80c3c34709115a6976e7d8b8786
Len Li <leonard.li@gmail.com>
2014-04-30 15:22:15 -0400

    Create VC's for storyboard

---
+++ Alice/LCLTicketDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LCLTicketDetailViewController.h
+//  Alice
+//
+//  Created by Leonard Li on 4/30/14.
+//  Copyright (c) 2014 Leonard Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LCLTicketDetailViewController : UIViewController
+
+@end
